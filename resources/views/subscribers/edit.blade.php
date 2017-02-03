086a475a882d335e4dac8397817e9c74576a04b0
vera.ko <vera.kopylchuk@gmail.com>
2017-02-03 23:25:59 +0200

    add edit subscriber

---
+++ resources/views/subscribers/edit.blade.php
@@ -0,0 +1,74 @@
+
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="container">
+   <div class="row">
+       <div class="col-md-8 col-md-offset-2">
+           <div class="panel panel-default">
+               <div class="panel-heading">Subscriber</div>
+               <div class="panel-body">
+               		<h3>Subscriber List</h3>
+					<a href="subscribers/create">Add New</a>
+					
+					<form class="form-horizontal" role="form" method="post" action="{{ url('/subscribers/'.$id) }}">
+						<input type="hidden" name="_method" value="put">
+						{{ csrf_field() }}
+						<div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
+						   <label for="first_name" class="col-md-4 control-label">First name</label>
+
+						   <div class="col-md-6">
+							   <input id="first_name" type="text" class="form-control" name="first_name" value="{{ $first_name or old('first_name') }}" required autofocus>
+
+							   @if ($errors->has('first_name'))
+							   <span class="help-block">
+								   <strong>{{ $errors->first('first_name') }}</strong>
+							   </span>
+							   @endif
+						   </div>
+						</div>
+						
+						<div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }}">
+						   <label for="last_name" class="col-md-4 control-label">Last Name</label>
+
+						   <div class="col-md-6">
+							   <input id="last_name" type="text" class="form-control" name="last_name" required value="{{ $last_name }}">
+
+							   @if ($errors->has('last_name'))
+								   <span class="help-block">
+								   <strong>{{ $errors->first('last_name') }}</strong>
+							   </span>
+							   @endif
+						   </div>
+						</div>
+						<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+						   <label for="last_name" class="col-md-4 control-label">E-Mail Address</label>
+
+						   <div class="col-md-6">
+							   <input id="email" type="email" class="form-control" name="email" required value="{{$email or old('email') }}">
+
+							   @if ($errors->has('email'))
+								   <span class="help-block">
+								   <strong>{{ $errors->first('email') }}</strong>
+							   </span>
+							   @endif
+						   </div>
+						</div>
+						<div class="form-group">
+						   <div class="col-md-8 col-md-offset-4">
+							   <button type="submit" class="btn btn-primary">
+								   EDIT
+							   </button>
+						   </div>
+						</div>
+					</form>
+               </div>
+           </div>
+       </div>
+   </div>
+</div>
+
+ 
+@endsection
